d5f58d0dfcc56b6fed9a06a7deda0790669acef5
Arthur Akira <arthurakira.dev@gmail.com>
2022-11-04 19:03:50 -0300

    create user route

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { UsersModule } from './users/users.module';
+import { ConfigModule } from '@nestjs/config';
 
+ConfigModule.forRoot();
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [UsersModule, MongooseModule.forRoot(process.env.MONGO_URL)],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
